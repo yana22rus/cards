3fd6a22b16db7935dbcc2d773288fed01c7d09d7
qwe <you@example.com>
2022-04-11 00:45:01 +0300

    add render list

--- main.go
+++ main.go
@@ -1,48 +1,87 @@
 package main
 
 import (
-	"net/http"
-	"text/template"
+  "net/http"
+  "text/template"
+  "fmt"
+
 )
 
 var tpl *template.Template
 
 func init() {
 
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+  tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 
 }
 
 func main() {
 
-	http.HandleFunc("/", create_card)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.ListenAndServe(":8000", nil)
+  http.HandleFunc("/", create_card)
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+  http.ListenAndServe(":8000", nil)
 
 }
 
-type Card struct {
-	Title       string
-	Description string
-	Class       []string
-	Type        string
-	Rare        string
-	Mana        int
-	Attack      int
-	Life        int
-	Keyword     string
-}
+
 
 type card struct {
-	Class1 []string
+  Class_card []string
+  Type_card []string
+  Rare_card []string
+  Attack_card []int
+  Mana_card []int
+  Life_card []int
+
 }
+
+
+
 
 func create_card(w http.ResponseWriter, r *http.Request) {
 
-	aaa := card{[]string{"s", "a", "w"}}
 
-	xp := []card{aaa}
 
-	tpl.ExecuteTemplate(w, "index.gohtml", xp)
+
+	mana := []int{}
+
+for i := 0; i<=20;i++{
+
+  mana = append(mana,i)
+
+}
+
+
+	attack := []int{}
+
+for i := 0; i<=20;i++{
+
+  attack = append(attack,i)
+
+}
+
+	life := []int{}
+
+for i := 1; i<=20;i++{
+
+  life = append(life,i)
+
+}
+
+  class := card{[]string{"Strength", "Intelligence", "Willpower","Agility","Endurance","Netural"},[]string{"Creature","Action","Item","Support"},[]string{"Legendary","Epic","Rare","Common"},attack,mana,life,}
+  
+
+
+  xp := []card{class}
+
+  tpl.ExecuteTemplate(w, "index.gohtml", xp)
+  
+  	if r.Method != "POST" {
+		
+		input := r.FormValue("title_card")
+		fmt.Println(input)
+
+
+	}
 
 }
