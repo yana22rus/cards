23a84d688f2381abd5b56b4681b3e88897593ffd
qwe <you@example.com>
2022-04-11 00:46:44 +0300

    go fmt

--- main.go
+++ main.go
@@ -1,86 +1,72 @@
 package main
 
 import (
-  "net/http"
-  "text/template"
-  "fmt"
-
+	"fmt"
+	"net/http"
+	"text/template"
 )
 
 var tpl *template.Template
 
 func init() {
 
-  tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 
 }
 
 func main() {
 
-  http.HandleFunc("/", create_card)
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-  http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", create_card)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8000", nil)
 
 }
-
-
 
 type card struct {
-  Class_card []string
-  Type_card []string
-  Rare_card []string
-  Attack_card []int
-  Mana_card []int
-  Life_card []int
-
+	Class_card  []string
+	Type_card   []string
+	Rare_card   []string
+	Attack_card []int
+	Mana_card   []int
+	Life_card   []int
 }
-
-
-
 
 func create_card(w http.ResponseWriter, r *http.Request) {
 
-
-
-
 	mana := []int{}
 
-for i := 0; i<=20;i++{
+	for i := 0; i <= 20; i++ {
 
-  mana = append(mana,i)
+		mana = append(mana, i)
 
-}
-
+	}
 
 	attack := []int{}
 
-for i := 0; i<=20;i++{
+	for i := 0; i <= 20; i++ {
 
-  attack = append(attack,i)
+		attack = append(attack, i)
 
-}
+	}
 
 	life := []int{}
 
-for i := 1; i<=20;i++{
+	for i := 1; i <= 20; i++ {
 
-  life = append(life,i)
+		life = append(life, i)
 
-}
+	}
 
-  class := card{[]string{"Strength", "Intelligence", "Willpower","Agility","Endurance","Netural"},[]string{"Creature","Action","Item","Support"},[]string{"Legendary","Epic","Rare","Common"},attack,mana,life,}
-  
+	class := card{[]string{"Strength", "Intelligence", "Willpower", "Agility", "Endurance", "Netural"}, []string{"Creature", "Action", "Item", "Support"}, []string{"Legendary", "Epic", "Rare", "Common"}, attack, mana, life}
 
+	xp := []card{class}
 
-  xp := []card{class}
+	tpl.ExecuteTemplate(w, "index.gohtml", xp)
 
-  tpl.ExecuteTemplate(w, "index.gohtml", xp)
-  
-  	if r.Method != "POST" {
-		
+	if r.Method != "POST" {
+
 		input := r.FormValue("title_card")
 		fmt.Println(input)
-
 
 	}
 
